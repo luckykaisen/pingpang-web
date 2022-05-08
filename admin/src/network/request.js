f494d06426b37b2855f49c76678c88d602b70907
weikaisen <374362311@qq.com>
2022-05-08 21:44:01 +0800

    【魏凯森】保存

--- admin/src/network/request.js
+++ admin/src/network/request.js
@@ -2,7 +2,8 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8081',
+  // baseURL: 'http://localhost:8081',
+  baseURL: 'http://43.138.40.155',
   timeout: 10000
 })
 
